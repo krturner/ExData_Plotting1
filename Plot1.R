40c4414c944702c367a57158593fc35bcdbcdccc
Kristopher Turner <KTurner@ResoluteEnergy.com>
2016-01-11 13:45:58 -0700

    Updated code for plots 1 and 2

--- Plot1.R
+++ Plot1.R
@@ -1,8 +1,10 @@
 #Read the data 
-usage <- read.table('household_power_consumption.txt', header=TRUE, sep=';') 
+usage <- read.table('household_power_consumption.txt', header=TRUE, sep=';', stringsAsFactors = FALSE) 
+finaldata <- usage[usage$Date == '1/2/2007' | usage$Date == '2/2/2007',]
 
 #Make the histogram (with titles and all!) 
-hist(as.numeric(usage$Global_active_power), col='red', main='Global Active Power', xlab='Global Active Power (kilowatts)') 
+hist(as.numeric(finaldata$Global_active_power), col='red', main='Global Active Power', xlab='Global Active Power (kilowatts)') 
+
 #Save the plot to a .png file 
 dev.copy(png, 'Plot1.png') 
 dev.off() 
